45e2db137d5c79e8071a7fcf67b77ea851f327c9
kalemena <kalemena@free.fr>
2019-06-07 20:55:30 +0200

    Add FSA8S receiver tests

---
+++ src/00-discover-radio/fsa8s-test.ino/fsa8s-test.ino.ino
@@ -0,0 +1,27 @@
+/* ~%93-95 success rate from transmistted data frame.
+    This rate testing on 2111 data frame.
+    In total data frame, 2000 valid frame, 111 invalid frame measured. */
+
+#include "FlySkyiBus.h"
+
+FlySkyiBus iBus(2, 2); // RX, TX
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Welcome!");
+    iBus.begin(115200);
+}
+
+uint8_t i=0;
+
+void loop() {
+
+    iBus.read_serial();
+
+    for(i=0; i<10; i++){
+        Serial.print(iBus.get_channel(i), DEC);
+        Serial.print("\t");
+    }
+
+    Serial.print("\n");
+}
